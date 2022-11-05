e67dfeccc63476d7013f679c36c00d82afdaa778
mrsaibaandreas <andreasqp@gmail.com>
2022-11-05 19:46:25 +0200

    Modified .gitignore

---
+++ src/main/java/utils/FileHandler.java
@@ -0,0 +1,21 @@
+package utils;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+public class FileHandler {
+    //writing to "LastGameState.txt" the players score, 
+    //remaining resources and number of trades
+    FileHandler() {
+        System.out.println("Dasda");
+    }
+    public static void main(String[] args)
+    {
+        FileHandler fl = new FileHandler();
+    }
+
+}
