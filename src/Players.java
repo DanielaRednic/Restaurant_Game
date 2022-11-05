34d00620d5466ba099a9ad81bb9a0df5c64b38a0
Daniela Livia Rednic <rednic.dana20@gmail.com>
2022-11-05 23:23:23 +0200

    Added logging

--- src/Players.java
+++ src/Players.java
@@ -4,7 +4,9 @@ public class Players {
         Resources resObj = new Resources();
         int player1[] = {0, 0, 0, 0, 0, 0};
         int player2[] = {0, 0, 0, 0, 0, 0};
+        System.out.println("Giving players resources...");
         resObj.getPlayerResources(player1);
+        System.out.println();
         resObj.getPlayerResources(player2);
     }
 }
