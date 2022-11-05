bc95eb78d472a49312912c4219c6107ec72bf22e
Daniela Livia Rednic <rednic.dana20@gmail.com>
2022-11-05 23:21:40 +0200

    Added methods and comments

--- src/Players.java
+++ src/Players.java
@@ -1,3 +1,10 @@
 public class Players {
-    //
+
+    public Players() {
+        Resources resObj = new Resources();
+        int player1[] = {0, 0, 0, 0, 0, 0};
+        int player2[] = {0, 0, 0, 0, 0, 0};
+        resObj.getPlayerResources(player1);
+        resObj.getPlayerResources(player2);
+    }
 }
