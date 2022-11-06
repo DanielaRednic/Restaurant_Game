6515d7af535692d80189e96ef200808c1eae2adb
Vlad <zerodivideone@gmail.com>
2022-11-06 09:53:44 +0200

    Modified main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,14 @@
 public class main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        System.out.println("##### Starting Game #####");
+        System.out.println("##### Game time: 90s ####");
+        Thread t = new Thread();
+        t.start();
+        try{
+            t.join(90000);
+        }catch(InterruptedException e){
+            System.out.println(e);
+        }
+        System.out.println("##### Player 1 Wins ####");
     }
 }
